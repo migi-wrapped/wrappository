2f5901b7d54e92acdd2abffe0cc8427a67c3d102
Alain <alain.zoja@hogalog.ch>
2025-11-20 23:10:40 +0100

    Add license files for Splide.js and vendors.js
    - Created LICENSE.txt for Splide.js version 4.1.4 under MIT license.
    - Created LICENSE.txt for vendors.js including licenses for Papa Parse v5.5.3 and @kurkle/color v0.3.4 under MIT license.

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE_NAME="shopping-wrapped-v1",ASSETS=["./","./index.html","./main.bundle.js"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(e=>e.addAll(ASSETS)))}),self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(t=>t||fetch(e.request)))});
